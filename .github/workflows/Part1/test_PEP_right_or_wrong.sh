8b1fd699f27b96a86106a44b91a1b03ce607d4fd
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2024-05-27 10:41:12 +0200

    add test script

--- .github/workflows/Part1/test_PEP_right_or_wrong.sh
+++ .github/workflows/Part1/test_PEP_right_or_wrong.sh
@@ -1,3 +1,11 @@
 #/bin/bash
 
-diff Part1/PEP_right_or_wrong.md .tests/Part1/PEP_right_or_wrong.md
+diff ../../chapter1/PEP_right_or_wrong.md ./Part1/PEP_right_or_wrong.md 
+no_lines=$(diff ../../chapter1/PEP_right_or_wrong.md ./Part1/PEP_right_or_wrong.md | wc -l)
+no_errors=$(echo "$no_lines/4" | bc)
+echo "There are $no_errors errors in your answer to PEP_right_or_wrong.md"
+if [ "$no_errors" -eq "0" ] ; then
+    echo "Well done!"
+else
+    echo "ERROR: Please check your answer and try again."
+fi 
