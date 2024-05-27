ea15e986aa751d0d0969def62a51b0a7d1d671ee
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2024-05-27 11:04:01 +0200

    clean up filepaths

--- .github/workflows/Part1/test_PEP_right_or_wrong.sh
+++ .github/workflows/Part1/test_PEP_right_or_wrong.sh
@@ -1,7 +1,8 @@
 #/bin/bash
-
-diff ../../chapter1/PEP_right_or_wrong.md ./Part1/PEP_right_or_wrong.md 
-no_lines=$(diff ../../chapter1/PEP_right_or_wrong.md ./Part1/PEP_right_or_wrong.md | wc -l)
+file_input=chapter1/PEP_right_or_wrong.md
+file_reference=.github/workflows/Part1/PEP_right_or_wrong.md
+diff "$file_input" "$file_reference"
+no_lines=$(diff "$file_input" "$file_reference" | wc -l)
 no_errors=$(echo "$no_lines/4" | bc)
 echo "There are $no_errors errors in your answer to PEP_right_or_wrong.md"
 if [ "$no_errors" -eq "0" ] ; then
